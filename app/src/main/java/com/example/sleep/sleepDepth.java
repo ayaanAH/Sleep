c540b51fa1533573c858b81ed56faf0b097f41bd
Ayaan Khan <ayaanalh10@gmail.com>
2019-05-19 11:34:47 +0530

    Adding a notification to the sleepDepth Calculator.
    Using the bigPictureStyle of notification.

--- app/src/main/java/com/example/sleep/sleepDepth.java
+++ app/src/main/java/com/example/sleep/sleepDepth.java
@@ -1,8 +1,17 @@
 package com.example.sleep;
 
+import android.annotation.TargetApi;
+import android.app.Notification;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.RequiresApi;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -20,6 +29,8 @@ public class sleepDepth extends AppCompatActivity
         weekDaysSleep=findViewById(R.id.weekdays);
         weekEndsSleep=findViewById(R.id.weekends);
         sleepDepth=findViewById(R.id.sleepdepth);
+
+
     }
 
     protected int checkDepth()
@@ -39,10 +50,12 @@ public class sleepDepth extends AppCompatActivity
     {
         int totalSleep = (wDays*5)+(wEnds*2);
         int totalDepth = reqSleep - totalSleep;
+        Log.i("DEPTH : ", String.valueOf(totalDepth));
         return totalDepth;
     }
 
 
+    @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
     public void checkResults(View view)
     {
         int result;
@@ -50,10 +63,43 @@ public class sleepDepth extends AppCompatActivity
         result = checkDepth();
         if (result>0)
             msg="You have slept "+result+"hrs less than recommended";
+        else if (result<-5)
+            msg = "GET UP! You are getting lazier everyday.You have slept "+result+" hours more than recommended";
         else
             msg="You have enough sleep";
         sleepDepth.setText(msg);
         sleepDepth.setTextColor(Color.RED);
         sleepDepth.setBackgroundResource(R.drawable.border);
+        NotifyDepth(msg);
+    }
+    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
+    public void NotifyDepth(String msg)
+    {
+
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this);
+        builder.setSmallIcon(R.drawable.sleepy);
+
+        Bitmap profilePicture = BitmapFactory.decodeResource(
+                this.getResources(),
+                R.drawable.sleepyhead
+        );
+
+        Bitmap bigPicture = BitmapFactory.decodeResource(
+                this.getResources(),
+                R.drawable.sleepyhead
+        );
+
+        NotificationCompat.BigPictureStyle style = new NotificationCompat.BigPictureStyle();
+        style.bigPicture(bigPicture);
+        style.setBigContentTitle("Get up lazy");
+        style.setSummaryText(msg);
+        style.bigLargeIcon(profilePicture);
+        builder.setStyle(style);
+
+        Notification notification = builder.build();
+        NotificationManagerCompat.from(this).notify(0 + 4, notification);
+
+
     }
 }
